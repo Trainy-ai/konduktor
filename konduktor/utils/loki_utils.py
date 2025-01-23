d896f409ce653468ca898d3bbe694be839e443ac
Andrew <asai@berkeley.edu>
2025-01-23 10:50:51 -0800

    color logtail

--- konduktor/utils/loki_utils.py
+++ konduktor/utils/loki_utils.py
@@ -1,13 +1,18 @@
+import colorama
 import json
 import time
 import urllib.parse
 from websocket import create_connection
 
-def tail_loki_logs_ws(job_name: str, worker_id: int = 0):
+from konduktor import logging
+
+logger = logging.get_logger(__name__)
+
+def tail_loki_logs_ws(job_name: str, worker_id: int = 0, num_logs: int = 100):
     loki_url = "ws://localhost:3100/loki/api/v1/tail"
     params = {
         "query": urllib.parse.quote(f'{{k8s_namespace_name="default"}} | jobset_sigs_k8s_io_jobset_name = `{job_name}` | batch_kubernetes_io_job_completion_index = `{worker_id}`'),
-        "limit": 10,
+        "limit": num_logs,
         "start": 1735746377, # this is Jan 1, 2025,
     }
     
@@ -15,16 +20,19 @@ def tail_loki_logs_ws(job_name: str, worker_id: int = 0):
     loki_url += f"?{query_string}"
 
     ws = create_connection(loki_url)
-    print("Connected to Loki WebSocktet")
+    logger.info(f"{colorama.Fore.YELLOW}Tailing logs from Loki. Press Ctrl+C to stop.{colorama.Style.RESET_ALL}")
 
     try:
         while True:
             message = ws.recv()
             payload = json.loads(message)
+            if len(payload['streams']) == 0:
+                logger.info(f"{colorama.Fore.YELLOW}No logs found for {job_name}{colorama.Style.RESET_ALL}")
+                break
             for stream in payload['streams']:
                 if stream['values'][0][1] is not None:
-                    print(stream['values'][0][1])
-            time.sleep(1)
+                    print(f"{colorama.Fore.CYAN}{colorama.Style.BRIGHT}(job_name={job_name} worker_id={worker_id}) {colorama.Style.RESET_ALL}{stream['values'][0][1]}")
+            time.sleep(5)
     except KeyboardInterrupt:
         print("Exiting...")
     finally:
